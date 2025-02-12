d408461493a66c2d26854a4f15a90d3cb63a404c
Al Emran <alemrangazi@gmail.com>
2025-02-12 21:24:42 +0600

    topbar

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import homeIcon from "./homeIcon.svg";
 import bag from "./bag.svg";
 import squareShape from "./square_shape.svg";
@@ -5,5 +6,25 @@ import forms from "./forms.svg";
 import components from "./components.svg";
 import elements from "./elements.svg";
 import settings from "./settings.svg";
+import { CiSearch } from "react-icons/ci";
+import { RiMenu2Line } from "react-icons/ri";
+import { IoSunny } from "react-icons/io5";
+import { GrPaint } from "react-icons/gr";
+import { PiSquaresFour } from "react-icons/pi";
+import { HiOutlineBell } from "react-icons/hi";
 
-export { bag, components, elements, forms, homeIcon, settings, squareShape };
+export {
+  bag,
+  components,
+  elements,
+  forms,
+  homeIcon,
+  settings,
+  squareShape,
+  CiSearch,
+  RiMenu2Line,
+  IoSunny,
+  GrPaint,
+  HiOutlineBell,
+  PiSquaresFour,
+};
--- src/components/Layout/TopBar/index.tsx
+++ src/components/Layout/TopBar/index.tsx
@@ -1,6 +1,50 @@
+import {
+  CiSearch,
+  GrPaint,
+  HiOutlineBell,
+  IoSunny,
+  PiSquaresFour,
+  RiMenu2Line,
+} from "../../../assets/icons";
 const TopBar = () => {
   return (
-    <div className="h-[60px] bg-white border-b border-[#CAD5E2]">TopBar</div>
+    <div className="h-[60px] bg-white border-b border-[#CAD5E2] flex flex-row justify-between items-center px-16">
+      {/* left */}
+      <div className="">
+        <RiMenu2Line size={26} className="text-[#4F46E5]" />
+      </div>
+      {/* right */}
+      <div className="flex flex-row gap-4 items-center">
+        {/* input field */}
+        <div className="relative">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Search Here"
+            className="bg-[#E9EEF5] pl-10 py-1 rounded-full outline-none"
+          />
+          <CiSearch
+            size={20}
+            className="absolute top-1/2 -translate-y-1/2 ml-3 text-[#5c6065]"
+          />
+        </div>
+        <div className="flex flex-row gap-1 items-center">
+          <button className="hover:bg-[#E9EEF5] p-2 rounded-full">
+            <IoSunny size={24} className="text-[#FFB900]" />
+          </button>
+          <button className="hover:bg-[#E9EEF5] p-2 rounded-full">
+            <GrPaint size={24} className="text-[#0092FF]" />
+          </button>
+          <button className="hover:bg-[#E9EEF5] p-2 rounded-full">
+            <HiOutlineBell size={24} className="" />
+          </button>
+          <button className="hover:bg-[#E9EEF5] p-2 rounded-full">
+            <PiSquaresFour size={24} />
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
