4d3bc5624eba0f68036df15d7af00a59364de8d0
Al Emran <alemrangazi@gmail.com>
2025-02-24 22:41:33 +0600

    routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,108 @@
+import {
+  applicationMenuListData,
+  componentsMenuListData,
+  dashboardMenuListData,
+  elementsMenuListData,
+  formsMenuListData,
+  pagesAndLayoutsMenuListData,
+} from "@/constant/menuListData.constant";
 import SideMenuData from "@/constant/SideMenuData.constant";
 import Layout from "@/Layout";
-
 import { Error404V1 } from "@/pages/pagesAndLayout/Error";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: <Error404V1 />,
-    children: SideMenuData.map((el) => ({
-      path: el.path,
+    children: SideMenuData.map(({ path }) => ({
+      path: path,
       element: (
-        <h2 className="text-5xl italic mx-auto text-center text-green-600 capitalize font-extrabold mt-20">
-          {el.title}
+        <h2 className="">
+          <Outlet />
         </h2>
       ),
+      children:
+        path === "/dashboards"
+          ? dashboardMenuListData.map(({ menu }) => ({
+              path: "",
+              element: (
+                <h1>
+                  <Outlet />
+                </h1>
+              ),
+              children: menu.map(({ path, title }) => ({
+                path: path,
+                element: <h2>{title}</h2>,
+              })),
+            }))
+          : path === "/applications"
+          ? applicationMenuListData.map(({ menu }) => ({
+              path: "",
+              element: (
+                <h1>
+                  <Outlet />
+                </h1>
+              ),
+              children: menu.map(({ path, title }) => ({
+                path: path,
+                element: <h1 className="">{title}</h1>,
+              })),
+            }))
+          : path === "/layouts"
+          ? pagesAndLayoutsMenuListData.map(({ menu }) => ({
+              path: "",
+              element: (
+                <h1>
+                  <Outlet />
+                </h1>
+              ),
+              children: menu.map(({ path, title }) => ({
+                path: path,
+                element: <h1 className="">{title}</h1>,
+              })),
+            }))
+          : path === "/forms"
+          ? formsMenuListData.map(({ menu }) => ({
+              path: "",
+              element: (
+                <h1>
+                  <Outlet />
+                </h1>
+              ),
+              children: menu.map(({ path, title }) => ({
+                path: path,
+                element: <h1 className="">{title}</h1>,
+              })),
+            }))
+          : path === "/components"
+          ? componentsMenuListData.map(({ menu }) => ({
+              path: "",
+              element: (
+                <h1>
+                  <Outlet />
+                </h1>
+              ),
+              children: menu.map(({ path, title }) => ({
+                path: path,
+                element: <h1 className="">{title}</h1>,
+              })),
+            }))
+          : path === "/elements"
+          ? elementsMenuListData.map(({ menu }) => ({
+              path: "",
+              element: (
+                <h1>
+                  <Outlet />
+                </h1>
+              ),
+              children: menu.map(({ path, title }) => ({
+                path: path,
+                element: <h1 className="">{title}</h1>,
+              })),
+            }))
+          : [],
     })),
   },
 ]);
